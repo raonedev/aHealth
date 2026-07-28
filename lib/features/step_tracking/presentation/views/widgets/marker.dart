aa4dcdbdba25dcdfc8f6f7558e87e400ecb6f74c
aman kumar <kumaraman33063@gmail.com>
2026-07-28 12:55:08 +0530

    update marker

--- lib/features/step_tracking/presentation/views/widgets/marker.dart
+++ lib/features/step_tracking/presentation/views/widgets/marker.dart
@@ -12,44 +12,76 @@ class RunnerMarker extends StatelessWidget {
   Widget build(BuildContext context) {
     final color = isMoving ? Colors.blue : Colors.grey;
 
-    return AnimatedContainer(
-      duration: const Duration(milliseconds: 250),
-      curve: Curves.easeOutCubic,
-      decoration: BoxDecoration(
-        color: color,
-        shape: BoxShape.circle,
-        border: Border.all(color: Colors.white, width: 2),
-        boxShadow: [
-          BoxShadow(
-            color: color.withValues(alpha: 0.5),
-            blurRadius: 12,
-            offset: const Offset(0, 2),
-          ),
-          BoxShadow(
-            color: Colors.black.withValues(alpha: 0.15),
-            blurRadius: 6,
-            offset: const Offset(0, 1),
-          ),
-        ],
+    return SizedBox(
+      width: 40,
+      height: 50,
+      child: CustomPaint(
+        painter: _PinPainter(color: color),
+        child: Center(
+          heightFactor: 0.65,
+          child: isMoving
+              ? Transform.rotate(
+                  angle: heading * math.pi / 180,
+                  child: const Icon(
+                    Icons.navigation_rounded,
+                    size: 14,
+                    color: Colors.white,
+                  ),
+                )
+              : Container(
+                  width: 8,
+                  height: 8,
+                  decoration: const BoxDecoration(
+                    color: Color(0xE6FFFFFF),
+                    shape: BoxShape.circle,
+                  ),
+                ),
+        ),
       ),
-      padding: const EdgeInsets.all(2),
-      child: isMoving
-          ? Transform.rotate(
-              angle: heading * math.pi / 180,
-              child: const Icon(
-                Icons.navigation_rounded,
-                size: 10,
-                color: Colors.white,
-              ),
-            )
-          : Container(
-              width: 8,
-              height: 8,
-              decoration: const BoxDecoration(
-                color: Color(0xE6FFFFFF),
-                shape: BoxShape.circle,
-              ),
-            ),
     );
   }
+}
+
+class _PinPainter extends CustomPainter {
+  _PinPainter({required this.color});
+  final Color color;
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final w = size.width;
+    final h = size.height;
+    final headRadius = w / 2;
+    final headCenter = Offset(w / 2, headRadius);
+
+    final path = Path()
+      ..moveTo(w / 2, h)
+      ..lineTo(w * 0.22, headRadius * 1.15)
+      ..arcToPoint(
+        Offset(w * 0.78, headRadius * 1.15),
+        radius: Radius.circular(headRadius),
+        clockwise: true,
+        largeArc: true,
+      )
+      ..close();
+
+    final shadowPaint = Paint()
+      ..color = Colors.black.withValues(alpha: 0.2)
+      ..maskFilter = const MaskFilter.blur(BlurStyle.normal, 3);
+    canvas.drawPath(path.shift(const Offset(0, 1)), shadowPaint);
+
+    final fillPaint = Paint()..color = color;
+    canvas.drawPath(path, fillPaint);
+
+    final borderPaint = Paint()
+      ..color = Colors.white
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = 2;
+    canvas.drawPath(path, borderPaint);
+
+    canvas.drawCircle(headCenter, headRadius - 1, fillPaint);
+    canvas.drawCircle(headCenter, headRadius - 1, borderPaint);
+  }
+
+  @override
+  bool shouldRepaint(covariant _PinPainter oldPainter) => oldPainter.color != color;
 }
